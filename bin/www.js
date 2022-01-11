514d837756253358dba9ee50f14f903b853cff44
kkm0406 <50093283+kkm0406@users.noreply.github.com>
2022-01-11 21:22:25 +0900

    Kwangmo_0111_2

--- bin/www.js
+++
@@ -1,6 +0,0 @@
-const port = 3002;
-const server = require("../server/server");
-
-server.listen(port, () => {
-  console.log(`listening on port ${port}`)
-});
--- server/routes/index.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ctrl = require("./ctrl");
-
-router.get('/qwer', ctrl.output.main);
-module.exports = router;
