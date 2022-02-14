d78d991aeb9293db2afa6835d04c534198e2af88
MINSU <ancx1234@naver.com>
2022-02-14 17:00:33 +0900

    0214
    homeCSS conflicts

--- src/Components/home/Home.jsx
+++ src/Components/home/Home.jsx
@@ -357,26 +357,15 @@ export const Home = () => {
         </div>
 
         <div className="menuList">
-          <NavLink
-            className="lists"
-            to="/playoff"
-            title="플레이오프 페이지 이동"
-          >
-            Play-Off
-          </NavLink>
+          <NavLink className="lists" to="/playoff" title="플레이오프 페이지 이동"> Play-Off</NavLink>
+
           <NavLink className="lists" to="/table" title="순위 페이지 이동">
             RANK
           </NavLink>
           <NavLink className="lists" to="/team" title="팀 정보 페이지 이동">
             TEAM
           </NavLink>
-          <NavLink
-            className="lists"
-            to="/players"
-            title="선수 정보 페이지 이동"
-          >
-            PLAYERS
-          </NavLink>
+          <NavLink className="lists" to="/players" title="선수 정보 페이지 이동" >PLAYERS</NavLink>     
         </div>
         {limitWidth &&
           <div className="images">
