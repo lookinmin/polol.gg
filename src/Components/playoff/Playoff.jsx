3f53d6d3eecdc050010ac2a75d202cd138605c6d
styughjvbn <3338761@naver.com>
2022-02-14 20:46:00 +0900

    playofff

--- src/Components/playoff/Playoff.jsx
+++ src/Components/playoff/Playoff.jsx
@@ -9,21 +9,43 @@ export const Playoff = () => {
   const [pic5, setPic5] = useState("img/LCK_MARK.png");
   const [pic6, setPic6] = useState("img/LCK_MARK.png");
   var playoff = new Array();
+  const [curmatch,setcurmatch]=useState(1);
+  var cur=1;
+
+  const [temp,settemp]=useState(false);
 
   useEffect(()=> {
     const callApi = async () =>{
       const res = await axios.get("http://localhost:3002/playoff");
       makeData(res.data.data);
     };
-
-  
     const makeData = (items) => {
-      playoff[0] = items[0].rank1;
-      playoff[1] = items[0].rank2;
-      playoff[2] = items[0].rank3;
-      playoff[3] = items[0].rank4;
-      playoff[4] = items[0].rank5;
-      playoff[5] = items[0].rank6;
+      cur=items.length-1;
+      playoff[2] = items[items.length-1].rank3;
+      playoff[3] = items[items.length-1].rank4;
+      playoff[4] = items[items.length-1].rank5;
+      playoff[5] = items[items.length-1].rank6;
+      if(cur>2){
+        var winner36;
+        if(items[0].LScore>items[0].RScore)
+          winner36=items[0].Lteam;
+        else
+          winner36=items[0].Rteam;
+        if(winner36==items[2].Lteam||winner36==items[2].Rteam){
+          playoff[1] = items[items.length-1].rank2;
+          playoff[0] = items[items.length-1].rank1;
+        }
+        else{
+          playoff[0] = items[items.length-1].rank2;
+          playoff[1] = items[items.length-1].rank1;
+        }
+        
+      }
+      else if(cur>3){
+        
+      }
+      
+      
 
       const setPicture = (e) => {
         var result;
@@ -72,42 +94,21 @@ export const Playoff = () => {
       setPic6(setPicture(playoff[5]));
     }
 
-   
-
     callApi();
   }, []);
 
 
-  const renderLine = (
-    <>
-      <div className="MiddleLine" id='Mline1'><hr className='line'></hr></div>
-      <div className="MiddleLine" id='Mline2'><hr className='line'></hr></div>
-      <div className="MiddleLine" id='Mline3'><hr className='line'></hr></div>
-      <div className="MiddleLine" id='Mline4'><hr className='line'></hr></div>
-      <div className="MiddleLine" id='Mline5'><hr className='line'></hr></div>
-      <div className="MiddleLine" id='Mline6'><hr className='line'></hr></div>
-      <div className="MiddleLine" id='Mline7'><hr className='line'></hr></div>
-      <div className="MiddleLine" id='Mline8'><hr className='line'></hr></div>
-
-      <div className="VerticalLine" id='line7'></div>
-      <div className="VerticalLine" id='line8'></div>
-      <div className="VerticalLine" id='line9'></div>
-      <div className="VerticalLine" id='line10'></div>
-      <div className="VerticalLine" id='line11'></div>
-    </>
-  )
-
   return (
     <>
       <div className='PlayOFF'>
-        <div className="playoffTeam" id='rank3'><img className='tImg' src={pic3}></img></div>
-        <div className="playoffTeam" id='rank6'><img className='tImg' src={pic6}></img></div>
-        <div className="playoffTeam" id='rank2'><img className='tImg' src={pic2}></img></div>
-        <div className="playoffTeam" id='rank1'><img className='tImg' src={pic1}></img></div>
-        <div className="playoffTeam" id='rank4'><img className='tImg' src={pic4}></img></div>
-        <div className="playoffTeam" id='rank5'><img className='tImg' src={pic5}></img></div>
-
-        {renderLine}
+        <button onClick={()=>{settemp(!temp)}}>toggle</button>
+        <div className={"playoffTeam "+(temp?"win36":"rank3")} ><img className='tImg' src={pic3}></img></div>
+        <div className="playoffTeam rank6" ><img className='tImg' src={pic6}></img></div>
+        <div className={"playoffTeam "+ (temp?"win236":"rank2")} ><img className='tImg' src={pic2}></img></div>
+        <div className={"playoffTeam "+(temp?"win145":"rank1")} ><img className='tImg' src={pic1}></img></div>
+        <div className={"playoffTeam "+(temp?"win45":"rank4")} ><img className='tImg' src={pic4}></img></div>
+        <div className="playoffTeam rank5" ><img className='tImg' src={pic5}></img></div>
+        <img className='trophy' src='img/trophy.png'></img>
       </div>
 
       <div className="underForPredict">
