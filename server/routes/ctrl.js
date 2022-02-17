f4d13ce894be4bc8240f1f3d75c217afdb84e069
kkm0406 <50093283+kkm0406@users.noreply.github.com>
2022-02-17 18:42:58 +0900

    0217

--- server/routes/ctrl.js
+++ server/routes/ctrl.js
@@ -2,7 +2,7 @@ const DB = require('../DataBase/ReadDB');
 const WriteMatchResult = require('../DataBase/DB_Write/WriteMatchResult');
 const WriteTeam = require('../DataBase/DB_Write/WriteTeam');
 const WritePlayer = require('../DataBase/DB_Write/WritePlayer');
-const WriteBanPick = require('../DataBase/DB_Write/WriteBanPick');
+// const WriteBanPick = require('../DataBase/DB_Write/WriteBanPick');
 
 const output = {
 
