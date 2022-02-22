e0e8cf22f3bc35eea2a65dd2fb9fd1e088dae4b6
kkm0406 <50093283+kkm0406@users.noreply.github.com>
2022-02-23 00:07:59 +0900

    00:07
    시즌 동적으로 생성중

--- src/Components/rank/Seasons.jsx
+++ src/Components/rank/Seasons.jsx
@@ -5,6 +5,13 @@ import { Dropdown } from "react-bootstrap";
 export const Seasons = ({ nowSeason }) => {
   const [season, setSeason] = useState("2022 LCK 스프링");
 
+  const [totalSeason, setTotalSeason] = useState([
+    "2022 LCK 서머",
+    "2022 LCK 스프링",
+    "2021 LCK 서머",
+  ]);
+  // table에서 시즌 가져와야 함
+
   const ClickSeason = (e) => {
     nowSeason(e.target.innerText);
     setSeason(e.target.innerText);
@@ -13,13 +20,22 @@ export const Seasons = ({ nowSeason }) => {
   return (
     <>
       <Dropdown>
-        <Dropdown.Toggle id="dropdown-button-dark-example1" className="dropdownBtn" variant="secondary">
-        {season}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <Dropdown.Toggle
+          id="dropdown-button-dark-example1"
+          className="dropdownBtn"
+          variant="secondary"
+        >
+          {season}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
         </Dropdown.Toggle>
 
         <Dropdown.Menu variant="dark" className="dropdown-menu">
-          <Dropdown.Item className='dropdown-item' onClick={ClickSeason}>2022 LCK 서머</Dropdown.Item>
-          <Dropdown.Item className='dropdown-item' onClick={ClickSeason}>2022 LCK 스프링</Dropdown.Item>
+          {totalSeason.map((e) => {
+            return (
+              <Dropdown.Item className="dropdown-item" onClick={ClickSeason}>
+                {e}
+              </Dropdown.Item>
+            );
+          })}
         </Dropdown.Menu>
       </Dropdown>
     </>
