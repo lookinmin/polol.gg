de25f689070b39c687b5c00843eb58d01d48f8c3
kkm0406 <50093283+kkm0406@users.noreply.github.com>
2022-02-22 16:58:55 +0900

    16:48

--- src/Components/home/UnderLayer/Schedule.jsx
+++ src/Components/home/UnderLayer/Schedule.jsx
@@ -141,53 +141,53 @@ export const Schedule = () => {
   const apiData = async (today) => {
     var weekMatch = [];
     const res = await axios.get("http://localhost:3002/");
-    // const items = res.data.data;
-    // for (let i = 0; i < 45; i++) {
-    //   if (today <= items[i].Month * 100 + items[i].Day) {
-    //     for (let j = i; j < i + 1; j++) {
-    //       if (
-    //         today <= items[j].Month * 100 + items[j].Day &&
-    //         items[j].Month * 100 + items[j].Day <= today + 7
-    //       ) {
-    //         weekMatch.push({
-    //           matchDate: MakeNewDate(items[j].Month, items[j].Day),
-    //           Lteam1: items[j].Lteam1,
-    //           Rteam1: items[j].Rteam1,
-    //           score1L: items[i].Lscore1,
-    //           score1R: items[i].Rscore1,
-    //           Lteam2: items[j].Lteam2,
-    //           Rteam2: items[j].Rteam2,
-    //           score2L: items[i].Lscore2,
-    //           score2R: items[i].Rscore2,
-    //         });
-    //       }
-    //     }
-    //   }
-    // }
-    // setMatchSchedule(weekMatch);
-    
-    const items = res.data.Playoff;
-    for (let i = 0; i < items.length; i++) {
-      if (today <= Number(items[i].month * 100) + Number(items[i].day)) {
+    const items = res.data.data;
+    for (let i = 0; i < 45; i++) {
+      if (today <= items[i].Month * 100 + items[i].Day) {
         for (let j = i; j < i + 1; j++) {
           if (
-            today <= Number(items[i].month * 100) + Number(items[i].day) &&
-            Number(items[i].month * 100) + Number(items[i].day) <= today + 7
+            today <= items[j].Month * 100 + items[j].Day &&
+            items[j].Month * 100 + items[j].Day <= today + 7
           ) {
             weekMatch.push({
-              matchDate: MakeNewDate(items[j].month, items[j].day),
-              Lteam: items[j].Lteam,
-              LScore: items[j].LScore,
-              RScore: items[j].RScore,
-              Rteam: items[j].Rteam,
-              round: items[j].round
+              matchDate: MakeNewDate(items[j].Month, items[j].Day),
+              Lteam1: items[j].Lteam1,
+              Rteam1: items[j].Rteam1,
+              score1L: items[i].Lscore1,
+              score1R: items[i].Rscore1,
+              Lteam2: items[j].Lteam2,
+              Rteam2: items[j].Rteam2,
+              score2L: items[i].Lscore2,
+              score2R: items[i].Rscore2,
             });
           }
         }
       }
     }
     setMatchSchedule(weekMatch);
-    setIsPlayOff(true);
+    
+    // const items = res.data.Playoff;
+    // for (let i = 0; i < items.length; i++) {
+    //   if (today <= Number(items[i].month * 100) + Number(items[i].day)) {
+    //     for (let j = i; j < i + 1; j++) {
+    //       if (
+    //         today <= Number(items[i].month * 100) + Number(items[i].day) &&
+    //         Number(items[i].month * 100) + Number(items[i].day) <= today + 7
+    //       ) {
+    //         weekMatch.push({
+    //           matchDate: MakeNewDate(items[j].month, items[j].day),
+    //           Lteam: items[j].Lteam,
+    //           LScore: items[j].LScore,
+    //           RScore: items[j].RScore,
+    //           Rteam: items[j].Rteam,
+    //           round: items[j].round
+    //         });
+    //       }
+    //     }
+    //   }
+    // }
+    // setMatchSchedule(weekMatch);
+    // setIsPlayOff(true);
   };
 
   useEffect(() => {
@@ -210,7 +210,7 @@ export const Schedule = () => {
       (currentdate - oneJan) / (24 * 60 * 60 * 1000)
     );
     setTimeLineCnt(
-      Math.ceil((currentdate.getDay() + 1 + numberOfDays) / 7) - 2
+      Math.ceil((currentdate.getDay() + 1 + numberOfDays) / 7) - 1
     );
 
     if (week.length !== 0) {
